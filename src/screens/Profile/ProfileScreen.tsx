e2f0d966161527c3f1dd430e19013122e6eda025
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-11-02 14:37:52 +0200

    feat: create signOutHandler

--- src/screens/Profile/ProfileScreen.tsx
+++ src/screens/Profile/ProfileScreen.tsx
@@ -1,11 +1,20 @@
-import {Text, View} from 'react-native';
+import {Button, Text, View} from 'react-native';
 import React from 'react';
+import {useAppDispatch} from '../../common/hooks/hooks';
+import {signOut} from '../../store/authSlice';
 
 export function ProfileScreen() {
+
+    const dispatch = useAppDispatch()
+
+    const signOutHandler = () => {
+        dispatch(signOut())
+    }
 
     return (
         <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
             <Text>Profile Screen</Text>
+            <Button title={'Sign out'} onPress={signOutHandler}/>
         </View>
     );
 }
