96a4f5ea4ead2a7c9aa88563f152691d8499e592
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-11-02 14:33:42 +0200

    feat: wrap Main in Provider

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import {Main} from './src/screens/Main';
 import {NavigationContainer} from '@react-navigation/native';
+import {Provider} from 'react-redux';
+import {store} from './src/store/store';
 
 export default function App() {
     return (
-        <NavigationContainer>
-            <Main/>
-        </NavigationContainer>
+        <Provider store={store}>
+            <NavigationContainer>
+                <Main/>
+            </NavigationContainer>
+        </Provider>
     );
 }
