0fad6ccfdf231deb298ab4937de5abc23db97622
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-11-02 11:10:54 +0200

    feat: implement DiscountsScreen

---
+++ src/screens/Discounts/DiscountsScreen.tsx
@@ -0,0 +1,11 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+
+export function DiscountsScreen() {
+
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text>Discounts Screen</Text>
+        </View>
+    );
+}
