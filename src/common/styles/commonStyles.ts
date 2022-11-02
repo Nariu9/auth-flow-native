cf2a64b0792ddb63227694dd5cf3fa3a96feccf7
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-11-02 11:15:59 +0200

    feat: implement common styles file

---
+++ src/common/styles/commonStyles.ts
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+
+export const commonStyles = StyleSheet.create({
+    input: {
+        height: 40,
+        borderRadius: 10,
+        padding: 5,
+        width: '60%',
+        backgroundColor: '#fff'
+    },
+});
