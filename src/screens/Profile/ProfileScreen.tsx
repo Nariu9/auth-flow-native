c83737b852ececd7a7fd34fd111a75b4b654d963
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-11-02 11:11:39 +0200

    feat: implement ProfileScreen

---
+++ src/screens/Profile/ProfileScreen.tsx
@@ -0,0 +1,11 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+
+export function ProfileScreen() {
+
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text>Profile Screen</Text>
+        </View>
+    );
+}
